c69b52a6263fad31bc239a7446c30f8de9fc8768
piyush028 <goyalpiyush028@gmail.com>
2026-07-07 10:45:59 +0530

    SUB ARRAY SUM BRUTE

---
+++ SubArraySumequalsKbrute.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class SubArraySumequalsKbrute {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int count = 0;
+        for (int start = 0; start < n; start++) {
+            int sum = 0;
+            for (int end = start; end < n; end++) {
+                sum += arr[end];
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+        sc.close();
+    }
+}
